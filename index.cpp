b9f2370b06cf7d08f6d00383e5ff36e79d0abb3a
Artem <152091756+SmokeyBard@users.noreply.github.com>
2024-02-29 21:44:57 +0200

    final

--- index.cpp
+++ index.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+    cout << "N: ";
     int n;
     cin >> n;
 
-    for(int i = 0; i < n; i++)
-    
+    int a = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++) {
+            cout << "*";
+        }
+        for (int k = 1; k <= i; k++) {
+            cout << a;
+        
+        }
+        a++;
+        cout << endl;
+    }
 }
